9fae09c4713f5f5a103663a2c857dae734585fde
mister2dev <michelmaineime@hotmail.com>
2024-04-10 12:59:49 +0200

    signup et login ok

--- front/src/App.js
+++ front/src/App.js
@@ -2,11 +2,13 @@ import "./styles/App.css";
 import { Route, Routes } from "react-router-dom";
 import Profil from "./pages/Profil";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <div>
+        {/* <Navbar /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profil" element={<Profil />} />
