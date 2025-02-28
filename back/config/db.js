6faf4f8bdcc9c2d96c336c678a6f2af6ccb0e8d4
mister2dev <h2gveille@gmail.com>
2025-02-28 21:38:51 +0100

    maj db

--- back/config/db.js
+++ back/config/db.js
@@ -9,10 +9,10 @@ const pool = new Pool({
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: 5432, // port par défaut de PostgreSQL
-  // connectionString: process.env.DATABASE_URL,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
